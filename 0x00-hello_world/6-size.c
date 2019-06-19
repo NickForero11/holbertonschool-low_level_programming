de806393cb13874fc785be21d5bd19b931a0bd7b
Nick Forero <nicolasforero1199@gmail.com>
2019-06-19 22:01:36 +0000

    Fixed the 0th script and the 6 program(Missing flag and a typo)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	char msg[12] = "Size of a";
 
 	printf("%s %s: %d byte(s)\n", msg, "char", sizeof(char));
-	printf("%s %s: %d byte(s)\n", msg, "int", sizeof(int));
+	printf("%sn %s: %d byte(s)\n", msg, "int", sizeof(int));
 	printf("%s %s: %d byte(s)\n", msg, "long int", sizeof(long int));
 	printf("%s %s: %d byte(s)\n", msg, "long long int", sizeof(long long int));
 	printf("%s %s: %d byte(s)\n", msg, "float", sizeof(float));
