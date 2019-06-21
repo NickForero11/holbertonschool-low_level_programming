9792fdf0e0121e8adec1d3e1f1a5e003048b738c
Nick Forero <nicolasforero1199@gmail.com>
2019-06-21 04:43:28 +0000

    Added the advanced task 11(Pending for check)

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int num;
+	int aux;
+	int copia;
+
+	aux  = 49;
+	copia = aux;
+
+        for (num = 48; (num - 48) / 10 == 0; num++)
+        {
+                for ( ; (aux - 48) / 10 == 0; aux++)
+                {
+                        putchar(num);
+                        putchar(aux);
+                        if (!(aux == 57 && num == 56))
+                        {
+				putchar(',');
+                                putchar(' ');
+                        }
+                }
+		aux = copia + 1;
+		copia = aux;
+        }
+        putchar('\n');
+
+	return (0);
+}
