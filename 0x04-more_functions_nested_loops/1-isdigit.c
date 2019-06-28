b37d885c5b5faa8a569d2f49708c4c4386def738
Nick Forero <nickforero@pop-os.localdomain>
2019-06-27 23:14:00 -0500

    Added 7 task of the project 0x04-more_functions_nested_loops(Pending for check)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - Checks for a digit (0 through 9).
+ *
+ *@c: is the integer value that will be checked.
+ *
+ * Return: 1 if c is digit.
+ *         0 otherwise.
+ *
+ */
+int _isdigit(int c)
+{
+	int r;
+
+	r = 0;
+
+	if (c > 47 && c < 58)
+	{
+		r = 1;
+	}
+
+	return (r);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# This project contains several functions using nested loops in C language
+### Also every function of the project uses an custom header file with the prototypes of each one.
