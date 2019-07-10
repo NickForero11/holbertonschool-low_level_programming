d4241766a8fb2ea5b92296a4d708cf1a3999582d
Nick Forero <nickforero@pop-os.localdomain>
2019-07-10 14:28:38 -0500

    Fixed again the task 1.

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,12 +1,9 @@
-#ifndef holberton
-#define holberton
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
-
-
-
 int wildcmp(char *s1, char *s2);
 #endif
