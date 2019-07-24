130c8eac93cc200bef348bc3511b7e4a6a6602de
Nick Forero <nickforero@pop-os.localdomain>
2019-07-24 15:30:47 -0500

    Added the project 0x0F-function_pointers with the tasks 0 and 1(Pending for check) and README.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Takes a function to print an string and a string an do it.
+ *
+ *@name: the string(or char *) that will be printed.
+ *
+ *@f: the pointer to the function used to print @name.
+ *
+ * Return: Always nothing (void).
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (*name != '\0' || name != NULL)
+	{
+		(*f)(name);
+	}
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a parameter
+ *					on each element of an array.
+ *
+ *@array: the integer pointer OR integer array used as base of @action.
+ *
+ *@size: the size of @array.
+ *
+ *@action: the pointer to the function used on every element of @array.
+ *
+ * Return: Always nothing (void).
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && size != 0 && action != NULL)
+	{
+		for (size_t i = 0; i < size; i++)
+		{
+			(*action)(*(array + i));
+		}
+	}
+}
