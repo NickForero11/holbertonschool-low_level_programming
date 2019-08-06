b9a38f40f86fb97765dec311ca10b330dc96866b
Nick Forero <nickforero@pop-os.localdomain>
2019-08-06 12:51:56 -0500

    Fixed task 9(missing return when idx == 0).

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	iterator = *head;
-
 	new = malloc(sizeof(listint_t));
 
 	if (!new)
@@ -39,6 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *(head);
 		*head = new;
+		return (new);
 	}
 
 	for (i = 0; i < idx - 1; i++)
