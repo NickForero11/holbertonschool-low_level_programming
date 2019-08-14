85244a681c24cc95e5e6a88ec26b0144c920e6b5
NickForero11 <nicolasforero1199@gmail.com>
2019-08-13 19:36:13 -0500

    Fixed task 1 (not closing and returning when text_content is NULL).

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,15 +32,15 @@ int create_file(const char *filename, char *text_content)
 	{
 		len = _strlen(text_content);
 		writed = write(fd, text_content, len);
-		closed = close(fd);
+		if (writed != (unsigned int) len)
+		{
+			return (-1);
+		}
 	}
 
-	if (closed < 0 || writed != (unsigned int) len)
-	{
-		return (0);
-	}
+	closed = close(fd);
 
-	return (1);
+	return (closed < 0 ? -1 : 1);
 }
 
 /**
