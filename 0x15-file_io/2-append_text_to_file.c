482219d31a21996084ae24d5e83dc7cc29c4a001
NickForero11 <nicolasforero1199@gmail.com>
2019-08-13 19:59:33 -0500

    Added the task 2 and reviewed the documentation of the task 1(Pending for check).

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * create_file - Creates a file with certain content.
+ * append_text_to_file - Appends text at the end of a file.
  *
  *@filename: is the string of the path to the file that will be printed.
  *
  *@text_content: is a NULL terminated string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success and -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len;
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 	{
