be3e22a013e6afeca0c28ea84a53de9ce0066d17
NickForero11 <nicolasforero1199@gmail.com>
2019-09-16 02:03:48 -0500

    Added task 8 (Pending for check).

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,40 +16,42 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *iterator;
 	dlistint_t *future;
-	dlistint_t *past;
+	dlistint_t *trash;
 	unsigned int i;
-
 
 	if (head == NULL || *head == NULL)
 	{
 		return (-1);
 	}
-
 	iterator = *head;
-
 	if (index == 0)
 	{
 		iterator = iterator->next;
 		free(*head);
+		if (iterator)
+			iterator->prev = NULL;
 		*head = iterator;
 		return (1);
 	}
-
 	for (i = 0; i < index - 1; i++)
 	{
-		if (iterator->next == NULL)
-		{
+		if (iterator == NULL)
 			return (-1);
-		}
-
 		iterator = iterator->next;
 	}
-
-
-	future = iterator->next->next;
-	past = iterator;
-	free(iterator->next);
-	single_connector(past, future);
-
+	trash = iterator;
+	if (iterator->next)
+	{
+		trash = trash->next;
+		if (iterator->next->next)
+		{
+			future = iterator->next->next;
+			single_connector(iterator, future);
+		} else
+		{
+			trash->prev->next = NULL;
+		}
+		free(trash);
+	}
 	return (1);
 }
