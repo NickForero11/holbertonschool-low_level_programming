1f0df70ee97984a0015e8624a1b8854c3e6547ba
NickForero11 <nicolasforero1199@gmail.com>
2019-10-12 01:03:13 -0500

    Added task 1 (Pending for check).

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+* hash_djb2 - Hash generator function to get the index of every node.
+*
+* @str: the string that will be hashed to get it's index on the HashTable.
+*
+* Return: The hash value of the key for the HashTable.
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+
+	return (hash);
+}
