4077aacee240cad474aec366af140a20b5faeab3
NickForero11 <nicolasforero1199@gmail.com>
2019-10-12 00:31:44 -0500

    Added project 0x1A-hash_tables with README and task 0 (Pending for check).

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a HashTable in the heap.
+ *
+ * @size: the number of buckets that will store.
+ *
+ * Return: a pointer to the newly created hash table, otherwise NULL.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashTable;
+	hash_node_t **buckets;
+	unsigned long int i;
+
+	hashTable = malloc(sizeof(hash_table_t));
+	if (!hashTable)
+	{
+		return (0);
+	}
+
+	hashTable->size = size;
+	buckets = calloc(size, sizeof(hash_node_t *));
+	if (!buckets)
+	{
+		return (0);
+	}
+
+	hashTable->buckets = buckets;
+	return (hashTable);
+}
