652c7ad1e0f78669360e96e2038c2b86fc842663
NickForero11 <nicolasforero1199@gmail.com>
2019-10-14 23:58:24 -0500

    Added task 5 (Pending for check).

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints all the elements of a Hash Table
+ *					  in a format of a Python dictionary, it means
+ *					  in the order that they appear in the array of buckets.
+ *
+ * @ht: the pointer to the first bucket in the hashTable,
+ *		or the array of buckets of the hashTable.
+ *
+ * Return: always nothing (void).
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int bucket_index;
+	int key_index;
+	hash_node_t *iterator;
+
+	if (ht != NULL)
+	{
+		putchar('{');
+
+		for (bucket_index = 0; bucket_index < ht->size; bucket_index++)
+		{
+			iterator = (ht->array)[bucket_index];
+
+			if (iterator != NULL)
+			{
+				key_index = 0;
+
+				while (iterator != NULL)
+				{
+
+					if (key_index != 0)
+					{
+						printf(", ");
+					}
+
+					printf("'%s': '%s'", iterator->key, iterator->value);
+
+					iterator = iterator->next;
+					key_index++;
+
+				}
+			}
+		}
+
+		puts("}");
+	}
+}
