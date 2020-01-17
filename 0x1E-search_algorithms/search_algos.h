0c724fad75a90eb444ea457fc84d46d2cf5c7d49
NickForero11 <nicolasforero1199@gmail.com>
2020-01-17 00:52:28 -0500

    Added a C function implementing the binary search algorithm with a fix to return the first occurrence of the value if it has duplicates in the array

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,5 @@ int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
 int binary_search_driver(int *array, size_t left, size_t right, int value);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
