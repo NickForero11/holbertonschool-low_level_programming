6e6020df8f4e19fe31e286dcfc991788064e98bc
NickForero11 <nicolasforero1199@gmail.com>
2020-01-17 00:07:32 -0500

    Added a C function implementing the binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,88 @@
+#include "search_algos.h"
+
+/**
+ * print_array - Prints the content of an integer array.
+ *
+ * @array: The integer array that will be processed.
+ *
+ * @left: The starting element of the array.
+ *
+ * @right: The ending element of the array.
+ *
+ * Return: Always nothing (void).
+ */
+
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t iterator;
+
+	printf("Searching in array: ");
+	for (iterator = left; iterator < right; iterator++)
+	{
+		printf("%d%s", array[iterator], iterator < right - 1 ? ", " : "\n");
+	}
+}
+
+/**
+ * binary_search_driver -	Finds an integer inside an array
+ *					using the binary search algorithm.
+ *
+ * @array: The integer array that contains the data.
+ *
+ * @left: The left limit of the array.
+ *
+ * @right: The right limit of the array.
+ *
+ * @value: The integer value that will be searched.
+ *
+ * Return:	The first index where value is located, otherwise -1.
+ */
+
+int binary_search_driver(int *array, size_t left, size_t right, int value)
+{
+	if (right > left)
+	{
+		size_t middle;
+
+		middle = left + ((right - left) / 2);
+
+		print_array(array, left, right);
+		if (array[middle] == value)
+		{
+			return (middle);
+		}
+		else if (value < array[middle])
+		{
+			return (binary_search_driver(array, left, middle - 1, value));
+		}
+		else
+		{
+			return (binary_search_driver(array, middle + 1, right, value));
+		}
+	}
+
+	return (-1);
+}
+
+/**
+ * binary_search -	Finds an integer inside an array
+ *					using the binary search algorithm.
+ *
+ * @array: The integer array that contains the data.
+ *
+ * @size: The number of elements in the array that will be traversed.
+ *
+ * @value: The integer value that will be searched.
+ *
+ * Return:	The first index where value is located, otherwise -1.
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	if (array != NULL && size > 0)
+	{
+		return (binary_search_driver(array, 0, size, value));
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,7 @@
 
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
-
+void print_array(int *array, size_t left, size_t right);
+int binary_search_driver(int *array, size_t left, size_t right, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
